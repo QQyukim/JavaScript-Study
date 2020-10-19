40b5df5e12dd6e4ff78ce969f6c24c37d40fafae
QQyukim <namgyu9696@gmail.com>
2020-10-19 22:39:26 +0900

    각 입력란에 아무것도 적지 않고 Submit 버튼을 누를 시, 에러 화면이 나타남

--- Form Validator/main.js
+++ Form Validator/main.js
@@ -1,11 +1,10 @@
 const container = document.querySelector('.container');
-const inputField = document.querySelector('.input_field');
+// const inputField = document.querySelector('.input_field');
 const name = document.querySelector('#name_field');
 const email = document.querySelector('#email_field');
 const pw = document.querySelector('#pw_field');
 const confrimPw = document.querySelector('#confirmPw_field');
 const noti = document.querySelector('.noti');
-
 
 function init() {
     window.addEventListener('resize', resizeFunc);
@@ -22,20 +21,25 @@ function resizeFunc() {
 }
 
 function submitForm() {
-    checkUserName();
+    checkAnswers(name);
+    checkAnswers(email);
+    checkAnswers(pw);
+    checkAnswers(confrimPw);
 }
 
-function checkUserName() {
-    if (name.value === "") {
-        inputField.classList.add('alert');
-        inputField.classList.remove('correct');
+function checkAnswers(input) {
+    const userInfo = input.parentNode;
+    const noti = userInfo.querySelector('.noti');
+
+    if (input.value === "") {
+        input.classList.add('alert');
+        input.classList.remove('correct');
         noti.style.display = 'block';
 
     } else {
-        inputField.classList.remove('alert');
-        inputField.classList.add('correct');
+        input.classList.remove('alert');
+        input.classList.add('correct');
         noti.style.display = 'none';
-
     }
 }
 
