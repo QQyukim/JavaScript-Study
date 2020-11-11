7de582f1de221f5af103d35f98143c2e7382e626
QQyukim <namgyu9696@gmail.com>
2020-11-11 19:22:51 +0900

    - 구 버전 코드 주석처리 - resize() 함수 삽입

--- Borame Park Quiz/main.js
+++ Borame Park Quiz/main.js
@@ -1,151 +1,182 @@
+const wrap = document.querySelector('#wrap');
 const container = document.querySelector('.container');
-const quizDisplay = document.querySelector('.quiz');
-const submitBtn = document.querySelector('#submit');
-const previousBtn = document.querySelector('#previous');
-const nextBtn = document.querySelector('#next');
-// const resultDisplay = document.querySelector('.result');
-const quizData = [
-    {
-        question : "보라매공원의 '보라매'는 무엇을 상징할까요?",
-        answers : {
-            a : "한국매보호협회",
-            b : "보랏빛 깃털을 가진 매",
-            c : "관악구의 웅장한 기개",
-            d : "공군사관학교"
-        },
-        correct : "d"
-    },
-    {
-        question : "에어파크에 전시되어 있지 않은 비행기 종류는?",
-        answers : {
-            a : "수송기",
-            b : "여객기",
-            c : "전투기",
-            d : "헬기"
-        },
-        correct : 'b'
-    },
-    {
-        question : "음악분수를 감상할 수 없는 달은?",
-        answers : {
-            a : '5월',
-            b : '8월',
-            c : '10월',
-            d : '사시사철 감상 가능',
-        },
-        correct : 'c'
-    }
-]
-let currentSlide = 0;
+const mainTitle = document.querySelector('.main-title');
+const subTitle = document.querySelector('.sub-title');
+const quizDescription = document.querySelector('.quiz-description');
+const startBtnWrap = document.querySelector('.start-btn-wrap');
+const startBtn = document.querySelector('.start-btn');
 
 function init() {
     window.addEventListener('resize', resize);
-    buildQuiz();
-	showSlide(currentSlide);
 }
 
 function resize() {
-    container.style.display = 'block';
+    container.style.display = "block";
+    let windowWidth = window.innerWidth;
 
-    let containerLeft = (window.innerWidth - container.clientWidth) / 2;
-    let containerTop = (window.innerHeight - container.clientHeight) / 2;
-
-    if (window.innerWidth > 750) {
-        container.style.marginLeft = containerLeft + 'px';
-    } else {
-        container.style.marginLeft = 0;
+    if (windowWidth < 768) {  // mobile
+        container.style.width = (600 * windowWidth) / 768 + "px";
+        quizDescription.style.fontSize = "14px";
+    } else if (windowWidth >= 768 && windowWidth < 1024) {  // tablet
+        container.style.width = (600 * windowWidth) / 768 + "px";
+        quizDescription.style.fontSize = "15px";
+        
+    } else if (windowWidth >= 1024) { // pc
+        container.style.width = (600 * 1024) / 768 + "px";
+        quizDescription.style.fontSize = "16px";
     }
-
-    if (window.innerHeight > 605) {
-        container.style.marginTop = containerTop + 'px';
-    } else {
-        container.style.marginTop = 0;
-    }
-
-}
-
-function buildQuiz() {
-    let output = []; //퀴즈 문제와 선택지가 저장된 배열
-
-    quizData.forEach(  //quizData배열값 불러오기
-        (currentQuestion, questionNum) => {
-            const answers = []; //퀴즈 선택지 배열
-
-            for(item in currentQuestion.answers){   // item = key
-                //퀴즈 선택지 DOM구조 생성
-                answers.push(`<label>
-                                <input class="eachAnswer" type="radio" name="question${questionNum}" value="${item}">
-                                ${currentQuestion.answers[item]}
-                            </label>`);
-                }
-
-                 //output배열에 퀴즈와 선택지 DOM 추가하기
-                output.push(`<div class="slide">
-                                <div class="question">${questionNum + 1}. ${currentQuestion.question}</div>
-                                <div class="answer">${answers.join('<br>')}</div>
-                            </div>`);
-            }
-        );
-        quizDisplay.innerHTML = output.join(' '); //join메서드, 퀴즈 사이에 공백 넣기
-        // quizDisplay.innerHTML = output; // ,(comma) 생성됨
-}
-
-function showSlide(n) {
-    const slides = document.querySelectorAll('.slide');
-
-    slides[currentSlide].classList.remove('active-slide');
-    slides[n].classList.add('active-slide');
-    currentSlide = n;
-
-    if (currentSlide === 0) {
-        previousBtn.style.display = 'none';
-    } else {
-        previousBtn.style.display = 'inline-block';
-    }
-
-    if (currentSlide === slides.length - 1) {
-        nextBtn.style.display = 'none';
-        submitBtn.style.display = 'inline-block';
-    } else {
-        nextBtn.style.display = 'inline-block';
-        submitBtn.style.display = 'none';
-    }
-}
-
-function showNextSlide() {
-    showSlide(currentSlide+1);
-}
-
-function showPreviousSlide(){
-    showSlide(currentSlide-1);
-}
-
-function showResult() {
-    //'answer'이름의 클래스를 배열로 저장하기
-    const answerDisplays = quizDisplay.querySelectorAll('.answer');
-    let numCorrect = 0; //퀴즈 정답률 기록
-
-    //답안 검증하기
-    quizData.forEach(
-        (currentQuestion, questionNum) => {
-            const answerDisplay = answerDisplays[questionNum]; //answerDisplays배열을 index별로 불러오기
-            const selector = `input[name=question${questionNum}]:checked`; //input태그의 속성값 지정하기
-            const userAnswer = (answerDisplay.querySelector(selector) || {}).value; //input check값 저장
-
-            if(userAnswer === currentQuestion.correct){  //user가 선택한 값과 정답 검증
-                numCorrect += 1;
-            }
-        }
-    );
-
-    previousBtn.style.display = 'none';
-    submitBtn.style.display = 'none';
-    
-    //resultDisplay DOM에 결과값 삽입하기
-    // resultDisplay.innerHTML = `${numCorrect} out of ${quizData.length}`;
 }
 
 init();
-previousBtn.addEventListener('click',showPreviousSlide);
-nextBtn.addEventListener('click',showNextSlide);
-submitBtn.addEventListener('click',showResult);
+
+// const container = document.querySelector('.container');
+// const quizDisplay = document.querySelector('.quiz');
+// const submitBtn = document.querySelector('#submit');
+// const previousBtn = document.querySelector('#previous');
+// const nextBtn = document.querySelector('#next');
+// // const resultDisplay = document.querySelector('.result');
+// const quizData = [
+//     {
+//         question : "보라매공원의 '보라매'는 무엇을 상징할까요?",
+//         answers : {
+//             a : "한국매보호협회",
+//             b : "보랏빛 깃털을 가진 매",
+//             c : "관악구의 웅장한 기개",
+//             d : "공군사관학교"
+//         },
+//         correct : "d"
+//     },
+//     {
+//         question : "에어파크에 전시되어 있지 않은 비행기 종류는?",
+//         answers : {
+//             a : "수송기",
+//             b : "여객기",
+//             c : "전투기",
+//             d : "헬기"
+//         },
+//         correct : 'b'
+//     },
+//     {
+//         question : "음악분수를 감상할 수 없는 달은?",
+//         answers : {
+//             a : '5월',
+//             b : '8월',
+//             c : '10월',
+//             d : '사시사철 감상 가능',
+//         },
+//         correct : 'c'
+//     }
+// ]
+// let currentSlide = 0;
+
+// function init() {
+//     window.addEventListener('resize', resize);
+//     buildQuiz();
+// 	showSlide(currentSlide);
+// }
+
+// function resize() {
+//     container.style.display = 'block';
+
+//     let containerLeft = (window.innerWidth - container.clientWidth) / 2;
+//     let containerTop = (window.innerHeight - container.clientHeight) / 2;
+
+//     if (window.innerWidth > 750) {
+//         container.style.marginLeft = containerLeft + 'px';
+//     } else {
+//         container.style.marginLeft = 0;
+//     }
+
+//     if (window.innerHeight > 605) {
+//         container.style.marginTop = containerTop + 'px';
+//     } else {
+//         container.style.marginTop = 0;
+//     }
+
+// }
+
+// function buildQuiz() {
+//     let output = []; //퀴즈 문제와 선택지가 저장된 배열
+
+//     quizData.forEach(  //quizData배열값 불러오기
+//         (currentQuestion, questionNum) => {
+//             const answers = []; //퀴즈 선택지 배열
+
+//             for(item in currentQuestion.answers){   // item = key
+//                 //퀴즈 선택지 DOM구조 생성
+//                 answers.push(`<label>
+//                                 <input class="eachAnswer" type="radio" name="question${questionNum}" value="${item}">
+//                                 ${currentQuestion.answers[item]}
+//                             </label>`);
+//                 }
+
+//                  //output배열에 퀴즈와 선택지 DOM 추가하기
+//                 output.push(`<div class="slide">
+//                                 <div class="question">${questionNum + 1}. ${currentQuestion.question}</div>
+//                                 <div class="answer">${answers.join('<br>')}</div>
+//                             </div>`);
+//             }
+//         );
+//         quizDisplay.innerHTML = output.join(' '); //join메서드, 퀴즈 사이에 공백 넣기
+//         // quizDisplay.innerHTML = output; // ,(comma) 생성됨
+// }
+
+// function showSlide(n) {
+//     const slides = document.querySelectorAll('.slide');
+
+//     slides[currentSlide].classList.remove('active-slide');
+//     slides[n].classList.add('active-slide');
+//     currentSlide = n;
+
+//     if (currentSlide === 0) {
+//         previousBtn.style.display = 'none';
+//     } else {
+//         previousBtn.style.display = 'inline-block';
+//     }
+
+//     if (currentSlide === slides.length - 1) {
+//         nextBtn.style.display = 'none';
+//         submitBtn.style.display = 'inline-block';
+//     } else {
+//         nextBtn.style.display = 'inline-block';
+//         submitBtn.style.display = 'none';
+//     }
+// }
+
+// function showNextSlide() {
+//     showSlide(currentSlide+1);
+// }
+
+// function showPreviousSlide(){
+//     showSlide(currentSlide-1);
+// }
+
+// function showResult() {
+//     //'answer'이름의 클래스를 배열로 저장하기
+//     const answerDisplays = quizDisplay.querySelectorAll('.answer');
+//     let numCorrect = 0; //퀴즈 정답률 기록
+
+//     //답안 검증하기
+//     quizData.forEach(
+//         (currentQuestion, questionNum) => {
+//             const answerDisplay = answerDisplays[questionNum]; //answerDisplays배열을 index별로 불러오기
+//             const selector = `input[name=question${questionNum}]:checked`; //input태그의 속성값 지정하기
+//             const userAnswer = (answerDisplay.querySelector(selector) || {}).value; //input check값 저장
+
+//             if(userAnswer === currentQuestion.correct){  //user가 선택한 값과 정답 검증
+//                 numCorrect += 1;
+//             }
+//         }
+//     );
+
+//     previousBtn.style.display = 'none';
+//     submitBtn.style.display = 'none';
+    
+//     //resultDisplay DOM에 결과값 삽입하기
+//     // resultDisplay.innerHTML = `${numCorrect} out of ${quizData.length}`;
+// }
+
+// init();
+// previousBtn.addEventListener('click',showPreviousSlide);
+// nextBtn.addEventListener('click',showNextSlide);
+// submitBtn.addEventListener('click',showResult);
