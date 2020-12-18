b2c48a07549c699407b240ccec631162791dfc82
QQyukim <namgyu9696@gmail.com>
2020-12-18 20:47:32 +0900

    날씨/위치 정보 가져오는 기능 구현 (클론코딩)

---
+++ NomadCoder-ChromeApp/js/weather.js
@@ -0,0 +1,57 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "f8c98d6e3538359a80e004a6f7a787d5";
+const COORDS = 'coords';
+
+function getWeather(lat, lng) {
+    // fetch() 메서드 안에는 가져올 데이터가 들어가면 됨
+    // then(): 데이터가 완전히 들어온 다음 함수 호출 
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    ).then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`
+    });
+}
+
+function saveCoords(coordsObj) {
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+    const latitude = position.coords.latitude;
+    const longtitude = position.coords.longtitude;
+    const coordsObj = {
+        latitude,
+        longtitude
+        // 객체의 key와 value 이름이 같을 때 위와 같이 쓸 수 있음
+    }
+    saveCoords(coordsObj);
+    getWeather(latitude, longtitude);
+}
+
+function handleGeoError(position) {
+    
+}
+
+function askForCoords() {
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError)
+}
+
+function loadCoords() {
+    const loadedCoords = localStorage.getItem(COORDS);
+    if (loadedCoords == null) {
+        askForCoords();
+    } else{
+        const parsedCoords = JSON.parse(loadedCoords);
+        getWeather(parsedCoords.latitude, parsedCoords.longtitude);
+    }
+}
+
+function init() {
+    loadCoords();
+}
+
+init();
